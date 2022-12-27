cef6d3e1326a8b107707899d42c843d6189868b9
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-27 15:36:24 +0530

    ADDED : Test Code to returns exception when CSV Header is Incorrect.

--- src/test/java/indiastatecensusanalyser/CensusAnalyserTest.java
+++ src/test/java/indiastatecensusanalyser/CensusAnalyserTest.java
@@ -15,6 +15,7 @@ public class CensusAnalyserTest {
     private static final String WRONG_STATE_CODE_CSV_FILE_PATH = "E:\\Projects\\intellijProjects\\IndianStatesCensusAnalyserProblem\\src\\main\\java\\resources\\IndianStateCode.csv";
     private static final String INDIAN_STATE_CODE_CSV_FILE_WITH_WRONG_TYPE = "E:\\Projects\\intellijProjects\\IndianStatesCensusAnalyserProblem\\src\\main\\java\\org\\example\\IndianStateCode.txt";
     private static final String INDIAN_STATE_CODE_WITH_WRONG_DELIMITER = "E:\\Projects\\intellijProjects\\IndianStatesCensusAnalyserProblem\\src\\main\\java\\org\\example\\IndianStateCodeDelimiter.csv";
+    private static final String INDIAN_STATE_CODE_FILE_WITH_WRONG_HEADER = "E:\\Projects\\intellijProjects\\IndianStatesCensusAnalyserProblem\\src\\main\\java\\org\\example\\IndianStateCodeHeader.csv";
 
     @Test
     public void givenIndianCensusCSVFile_Returns_CorrectRecords() {
@@ -130,4 +131,18 @@ public class CensusAnalyserTest {
             Assertions.assertEquals(StateCensusAnalyserException.CensusAnalyserException.ExceptionType.INCORRECT_HEADER_OR_DELIMITER, e.type);
         }
     }
+
+    @Test
+    public void givenIndianStateCSVFile_WithWrongFileHeader_ShouldThrowException() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(StateCensusAnalyserException.CensusAnalyserException.class);
+            stateCensusAnalyser.loadIndiaStateData(INDIAN_STATE_CODE_FILE_WITH_WRONG_HEADER);
+        } catch (StateCensusAnalyserException.CensusAnalyserException e) {
+            System.out.println(e.getMessage());
+            Assertions.assertEquals(StateCensusAnalyserException.CensusAnalyserException.ExceptionType.INCORRECT_HEADER_OR_DELIMITER, e.type);
+        }
+    }
+
 }
